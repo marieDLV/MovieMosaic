1157abc33cf695858a8c411b5b7be8a95c8e3ca0
Marie Delavalette <marie.delavalette@inetum.com>
2024-04-05 11:41:29 +0200

    bar de recherche automatique

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="search-bar">
-    <input type="text" v-model="query" @keyup.enter="search" placeholder="Rechercher un film..." />
+    <input type="text" v-model="query" @input="search" placeholder="Rechercher un film..." />
+    <button v-if="searchQuery" @click="clearSearch" class="clear-icon">X</button>
   </div>
 </template>
 
@@ -13,8 +14,60 @@ const emit = defineEmits(['results'])
 const query = ref('')
 
 const search = async () => {
+  // Only search if the query is at least 3 characters long
   const results = await searchMovies(query.value)
-  console.log(results)
+
   emit('results', results)
+  //emit('searchQuery', query.value)
+}
+
+const clearSearch = () => {
+  query.value = ''
+  emit('results', []) // Emit an empty array to clear the search results
+  //emit('searchQuery', '')
 }
 </script>
+<style scoped lang="scss">
+.search-bar {
+  display: flex;
+  justify-content: center;
+  padding: 10px;
+  background-color: var(--color-background);
+  position: relative;
+
+  input {
+    width: 50%;
+    padding: 10px;
+    border: none;
+    border-radius: 4px;
+    background-color: var(--color-secondary);
+    color: var(--color-text);
+    font-size: 16px;
+    transition: width 0.5s ease-in-out;
+
+    ::placeholder {
+      color: var(--color-text);
+    }
+
+    &:focus {
+      width: 70%;
+      outline: none;
+      background-color: var(--color-secondary);
+      color: var(--color-primary);
+
+      ::placeholder {
+        color: var(--color-primary);
+      }
+    }
+  }
+}
+.clear-icon {
+  position: absolute;
+  right: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+  background: none;
+  border: none;
+  cursor: pointer;
+}
+</style>
